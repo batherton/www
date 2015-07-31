d85dacdf9cd0acd34d561747706ea34c4781b67e
batherton <brian@asimail.com>
2015-07-31 03:13:49 -0400

    tracking1

--- js/index.js
+++ js/index.js
@@ -1,26 +1,5 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
 
 var app = {
-
-    // Application Constructor
 
     initialize: function() {
 
@@ -28,35 +7,29 @@ var app = {
 
     },
 
-    // Bind Event Listeners
-
-    //
-
-    // Bind any events that are required on startup. Common events are:
-
     // 'load', 'deviceready', 'offline', and 'online'.
 
     bindEvents: function() {
 
         document.addEventListener('deviceready', this.onDeviceReady, false);
 
+        document.addEventListener("offline", onOffline, false);
+        document.addEventListener("online", onOnline, false);
     },
 
-    // deviceready Event Handler
-
-    //
-
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-
-    // function, we must explicitly call 'app.receivedEvent(...);'
-
-    onDeviceReady: function() {
+    onDeviceReady: function() {
 
         app.receivedEvent('deviceready');
 
     },
 
-    // Update DOM on a Received Event
+
+    onOffline: function(){
+	//document.getElementById('NetworkConnectionType').value = "No network connection";
+    },
+    onOnline: function(){
+	//document.getElementById('NetworkConnectionType').value = "Online";
+    },
 
     receivedEvent: function(id) {
 
@@ -67,16 +40,14 @@ var app = {
         var receivedElement = parentElement.querySelector('.received');
 
 
-        listeningElement.setAttribute('style', 'display:none;');
+         listeningElement.setAttribute('style', 'display:none;');
 
-        receivedElement.setAttribute('style', 'display:block;');
+         receivedElement.setAttribute('style', 'display:block;');
 
 
-
-	document.getElementById('DeviceInfo').innerHTML = device.model+' '+device.platform+'<br> '+device.version+'<br>'+device.uuid+'<br>connection: '+navigator.connection.type;
-	//document.getElementById('ConnectionInfo').innerHTML = 'connection '+navigator.connection.type
-
-        console.log('Received Event: ' + id);
+	  document.getElementById('DeviceInfo').innerHTML = device.model+' '+device.platform+'<br> '+device.version+'<br>'+device.uuid+'<br>connection: '+navigator.connection.type;
+	  SetTrackLocation();
+    //    console.log('Received Event: ' + id);
 
     }
 
@@ -84,4 +55,43 @@ var app = {
 
 
 
-	navigator.geolocation.getCurrentPosition(geolocationSuccess,[geolocationError],[geolocationOptions]);
+//var watchID = navigator.geolocation.getCurrentPosition(onCurLocSuccess, onCurLocError, { maximumAge: 3000, enableHighAccuracy: true });
+
+function SetTrackLocation{
+var TrackLocation = navigator.geolocation.watchPosition(onCurLocSuccess, onCurLocError, { maximumAge: 3000, enableHighAccuracy: true });
+};
+
+function onCurLocSuccess(position) {
+    var element = document.getElementById('geolocation');
+    element.innerHTML = 'Latitude: '  + position.coords.latitude      + '<br/>' +
+                        'Longitude: ' + position.coords.longitude     + '<br/>' +
+		        'Altitude: '          + position.coords.altitude          + '<br/>' +
+		        'Accuracy: '          + position.coords.accuracy          + '<br/>' +
+        		'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '<br/>' +
+        		'Heading: '           + position.coords.heading           + '<br/>' +
+        		'Speed: '             + position.coords.speed             + '<br/>' +
+        		'Timestamp: '         + position.timestamp                + '<br/>'
+                       // '<hr />'      + element.innerHTML;
+};
+
+function onCurLocError(error) {
+    alert('code: '    + error.code    + '\n' +
+          'message: ' + error.message + '\n');
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
